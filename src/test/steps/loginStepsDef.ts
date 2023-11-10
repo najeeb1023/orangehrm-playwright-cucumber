205033fd80b38d7d6cccac8446403eeafe2f2930
Gul Najeeb Channa <najeeb.capricorn@gmail.com>
2023-11-10 18:21:11 +0100

    Timeperiod searching and then searching user from hidden dropdown.

--- src/test/steps/loginStepsDef.ts
+++ src/test/steps/loginStepsDef.ts
@@ -8,27 +8,21 @@ setDefaultTimeout(15000);
 let loginUserFunc = new Login(pageFixture.page);
 
 Given('the user lands at the webpage.', async function () {
-  
   await pageFixture.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  
   });
 
 When('User goes to the website and enters {string} and {string}.', async function (username: string, password: string) {
-  
   await loginUserFunc.loginUser(username, password);
 });
 
 When('User goes to the website and enters wrong {string} and {string}.', async function (username: string, password: string) {
-  
   await loginUserFunc.doesNotLoginUser(username, password);
 });
 
 Then('The user is logged in.', async function () {
-  
   await loginUserFunc.assertUserLogin();
 });
 
 Then('The user is not logged in.', async function () {
-  
   await loginUserFunc.assertUserNotLoggedIn();
-})
+});
--- src/test/steps/timesheetPageStepsDef.ts
+++ src/test/steps/timesheetPageStepsDef.ts
@@ -2,19 +2,20 @@ import { Given, When, Then, setDefaultTimeout } from "@cucumber/cucumber"
 import { TimeSheet } from "../pages/TimeSheetPage"
 import { pageFixture } from "../hooks/pageFixture"
 
-setDefaultTimeout(15000)
+setDefaultTimeout(15000);
 
 When('User navigates to Time Sheet page', async function (){
     let timeSheetFunc = new TimeSheet(pageFixture.page);
     await timeSheetFunc.goToTimeSheetAndEnterUser();
 
-})
+});
 
 Given('User is able to sort users', async function (){
     let timeSheetFunc = new TimeSheet(pageFixture.page);
     await timeSheetFunc.getEmployeeTimePeriod();
-})
+});
 
 Then('Time is returned for users', async function (){
-
-})
+    let timeSheetFunc = new TimeSheet(pageFixture.page);
+    await timeSheetFunc.getUsers();
+});
