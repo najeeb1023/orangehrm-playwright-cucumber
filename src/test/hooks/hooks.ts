eb0fb1558bad395908d2ca94c95af85d6a5e842a
Gul Najeeb Channa <najeeb.capricorn@gmail.com>
2023-11-17 19:57:47 +0100

    Git ignore fixed.

--- src/test/hooks/hooks.ts
+++ src/test/hooks/hooks.ts
@@ -9,13 +9,13 @@ let context: BrowserContext;
 
 BeforeAll(async function (){
     browser = await chromium.launch(config);
-})
+});
 
 Before(async function(){
     context = await browser.newContext();
     const page = await context.newPage();
     pageFixture.page = page;
-})
+});
 
 After(async function ({ pickle, result }){
     console.log(result?.status);
@@ -23,7 +23,8 @@ After(async function ({ pickle, result }){
         const img = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`,type:"png"});
         await this.attach(img, "image/png");
     }
-})
+});
+
 AfterAll(async function (){
     await browser.close();
-})
+});
